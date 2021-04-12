d3d920cc7595daff9ec539037d9d7653852cf7ce
Cahyo <cahyo.adi.permono@gmail.com>
2021-04-13 03:35:01 +0700

    adding type orm and its config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -16,6 +17,17 @@ import * as Joi from 'joi';
         DB_DATABASE: Joi.string().required(),
         PRIVATE_KEY: Joi.string().required(),
       }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: true,
+      entities: [],
     }),
   ],
   controllers: [AppController],
