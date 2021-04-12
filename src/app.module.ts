448596b7af37e87753ca48f47c3dd2a1ce136d65
Cahyo <cahyo.adi.permono@gmail.com>
2021-04-13 03:28:20 +0700

    adding nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
